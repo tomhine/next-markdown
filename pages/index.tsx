7d35ccb9f8b2409eabba97c2ef55070ee2754fd5
Tom Hine <tomhine792@gmail.com>
2022-06-07 23:27:20 +0100

    added main element with test sections for layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <main className="grid h-[calc(100vh-4rem)] w-full grid-cols-2 gap-[2px]">
+      <section className="bg-red-500"></section>
+      <section className="bg-blue-500"></section>
+    </main>
+  );
 };
 
 export default Home;
